f536f3dfbfc72be5e5e956f88691d21230edb608
GiovanniSperandini <sperandini.giovanni@gmail.com>
2025-04-01 10:07:25 +0200

    Add files via upload

---
+++ carte_da_poker.py
@@ -0,0 +1,14 @@
+seme_1=b"\xE2\x99\xA5"
+seme_2=b"\xE2\x99\xA3"
+seme_3=b"\xE2\x99\xA0"
+seme_4=b"\xE2\x99\xA6"
+seme_vuoto=b"\xE2\x99\xA1\xE2\x99\xA2\xE2\x99\xA4\xE2\x99\xA2"
+
+
+simbolo_1=seme_1.decode()
+simbolo_2=seme_2.decode()
+simbolo_3=seme_3.decode()
+simbolo_4=seme_4.decode()
+simbolo_5=seme_vuoto.decode()
+
+print(simbolo_1+simbolo_2+simbolo_3+simbolo_4+simbolo_5)
